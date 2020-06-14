9ace7529a4095a70676db0b961fbbe067c575d85
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-06-15 02:25:47 +0900

    make it deep

--- deep_hedging.py
+++ deep_hedging.py
@@ -10,77 +10,124 @@ import shutil
 
 plt.close('all')
 
+n_terms = 30
+n_weight = 10
+
 S_init = 100
+K = 100
+
 u = 0.1
 d = -0.05
+u = (1+u)**(1/n_terms)-1
+d = (1+d)**(1/n_terms)-1
 p = 0.6
 
 r = 0.01
-K = 100
+
+def binom_iter(input_size):
+    def input_generator():
+        while True:
+            input_list = [S_init*np.ones((input_size, ))]
+            for i in range(n_terms):
+                input_list.append((1+d+(u-d)*np.random.randint(2, size=(input_size, )))*input_list[-1])
+
+            target_payoff = np.maximum(input_list[-1]-K, 0).reshape((input_size, 1))
+            yield input_list, target_payoff
+    return 1, input_generator()
+
+class GeometricBrownianMotion:
+    def __init__(self, mu, sigma):
+        self.mu = mu
+        self.sigma = sigma
+
+    def get_path(self, time, spot):
+        dt = time[1:] - time[:-1]
+        S = np.insert((self.mu - 1/2*self.sigma**2)*dt \
+            + self.sigma*np.random.normal(0, np.sqrt(dt)), 0, np.log(spot)).cumsum()
+        
+        return np.exp(S)
+        # return pd.Series(np.exp(S), time)
+    
+    def get_generator(self, time, spot, input_size):
+        def input_generator():
+            while True:
+                input_np = np.stack([self.get_path(time, spot) for i in range(input_size)]).T
+                target_payoff = np.maximum(input_np[-1]-K, 0).reshape((input_size, 1))
+                yield [input_np[i] for i in range(input_np.shape[0])], target_payoff
+        return 1, input_generator()
+
+time = np.arange(0, n_terms+1)/360
+
+mu = 0.07
+sigma = 0.20
+
+stockModel = GeometricBrownianMotion(mu, sigma)
+
+
+N = 1000
+
+# r = (1+r)**(1/n_terms)-1
+# train_steps, train_batches = binom_iter(N)
+
+r = np.exp(r/360)-1
+train_steps, train_batches = stockModel.get_generator(time, S_init, N)
+
+
 
 # W_0, Vs_0を入力なしの変数にしたいが，
 # そうする方法がよくわからないので
 # 常に同じ値を入力するS_0を入力としている．
-S_0 = Input(shape=(1, ), name='S_0') # price of assets
-W_0 = Dense(1, use_bias=False, name='W_0')(S_0) # valuation of portfolio
-Vs_0 = Dense(1, use_bias=False, name='Vs_0')(S_0) # portfolio of stock by value
-Vr_0 = Subtract(name='Vr_0')([W_0, Vs_0]) # portfolio of risk-free asset by value
+S_list = [Input(shape=(1, ), name='S_0')] # price of assets
+W_list = [Dense(1, use_bias=False, name='W_0')(S_list[-1])] # valuation of portfolio
+Vs_list = [Dense(1, use_bias=False, name='Vs_0')(S_list[-1])] # portfolio of stock by value
+Vr_list = [Subtract(name='Vr_0')([W_list[-1], Vs_list[-1]])] # portfolio of risk-free asset by value
 
-S_1 = Input(shape=(1, ), name='S_1')
-Rs_1 = Lambda(lambda x: x[0]/x[1], name='Rs_1')([S_1, S_0]) # return of stock
-Ws_1 = Multiply(name='Ws_1')([Rs_1, Vs_0]) # valuation of stock after 1 term
-Wr_1 = (1+r)*Vr_0 # valuation of risk-free asset after 1 term
-W_1 = Add(name='W_1')([Ws_1, Wr_1])
-Vs_1 = Dense(1, use_bias=True, name='Vs_1')(W_1)
-Vr_1 = Subtract(name='Vr_1')([W_1, Vs_1])
+for i in range(1, n_terms+1):
+    S_list.append(Input(shape=(1, ), name='S_{}'.format(i)))
+    Rs_i = Lambda(lambda x: x[1]/x[0], name='Rs_{}'.format(i))(S_list[-2:]) # return of stock
+    Ws_i = Multiply(name='Ws_{}'.format(i))([Rs_i, Vs_list[-1]]) # valuation of stock after 1 term
+    Wr_i = (1+r)*Vr_list[-1] # valuation of risk-free asset after 1 term
+    W_list.append(Add(name='W_{}'.format(i))([Ws_i, Wr_i]))
+    if i < n_terms:
+        dense_i = Dense(n_weight, activation='relu', use_bias=True, name='Vs_{}_0'.format(i))(W_list[-1])
+        for j in range(1, min(i, 3)):
+            dense_i = Dense(n_weight, activation='relu', use_bias=True, name='Vs_{}_{}'.format(i, j))(dense_i)
+        Vs_list.append(Dense(1, use_bias=True, name='Vs_{}_{}'.format(i, i))(dense_i))
+        Vr_list.append(Subtract(name='Vr_{}'.format(i))([W_list[-1], Vs_list[-1]]))
 
-S_2 = Input(shape=(1, ), name='S_2')
-Rs_2 = Lambda(lambda x: x[0]/x[1], name='Rs_2')([S_2, S_1]) # return of stock
-Ws_2 = Multiply(name='Ws_2')([Rs_2, Vs_1]) # valuation of stock after 2 term
-Wr_2 = (1+r)*Vr_1 # valuation of risk-free asset after 2 term
-W_2 = Add(name='W_2')([Ws_2, Wr_2])
-
-model = Model(inputs=[S_0, S_1, S_2], outputs=W_2)
+model = Model(inputs=S_list, outputs=W_list[-1])
 
 # optimizer = keras.optimizers.RMSprop(lr=0.01)
 optimizer = keras.optimizers.Adam()
 
 model.compile(optimizer=optimizer, loss='mean_absolute_error')
 
-model_v = Model(inputs=[S_0, S_1, S_2], outputs=[W_0, Vs_0, Vr_0, Rs_1, Ws_1, Wr_1, W_1, Vs_1, Vr_1, Rs_2, Ws_2, Wr_2, W_2])
-model_option_price = Model(inputs=[S_0, S_1, S_2], outputs=W_0)
+model_option_price = Model(inputs=S_list, outputs=W_list[0])
 
 plot_model(model, show_shapes=True, to_file='model.png')
+model.summary()
 
-N = 100
-input0 = S_init*np.ones((N, ))
-input1 = (1+d+(u-d)*np.random.randint(2, size=(N, )))*input0
-input2 = (1+d+(u-d)*np.random.randint(2, size=(N, )))*input1
-input_list = [input0, input1, input2]
-
-target_payoff = np.maximum(input2-K, 0).reshape((N, 1))
 
 log_dir = '/tmp/log'
 shutil.rmtree(log_dir, ignore_errors=True)
 tensorboard_callback = keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
 
-history = model.fit(input_list, target_payoff, batch_size=100, epochs=20000, callbacks=[tensorboard_callback])
+# history = model.fit(input_list, target_payoff, batch_size=N, epochs=20000, callbacks=[tensorboard_callback])
+history = model.fit(train_batches, steps_per_epoch=train_steps, epochs=3000, callbacks=[tensorboard_callback])
 
 pd.DataFrame({'loss': history.history['loss']}).plot()
 plt.yscale('log')
 plt.grid()
 
+input_list, _ = next(train_batches)
+
 hedged_payoff = model.predict(input_list)
 plt.figure()
-plt.scatter(input2, hedged_payoff)
+plt.scatter(input_list[-1], hedged_payoff)
 plt.grid()
 
 option_price = model_option_price.predict(input_list)
-plt.figure()
-plt.hist(option_price)
-
-[input0[:2], input1[:2], input2[:2]]
-v = model_v.predict([input0[:2], input1[:2], input2[:2]])
+print(option_price[0][0])
 
 plt.show(block=False)
 
