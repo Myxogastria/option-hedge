96adbdb58afcb23a2e229884590f2fa34f323782
Myxogastria <50288647+Myxogastria@users.noreply.github.com>
2020-11-04 20:49:50 +0900

    plot of example

--- adversarial_example.py
+++ adversarial_example.py
@@ -9,8 +9,10 @@ hedged_payoff = model.predict(input_list)
 
 hedging_error = hedged_payoff - target_payoff
 
+n_pickup = 30
+
 # うまくいかなかった時系列
-worst_index = np.argsort(hedging_error, axis=0)[:10].reshape(-1, )
+worst_index = np.argsort(hedging_error, axis=0)[:n_pickup].reshape(-1, )
 pd.DataFrame({'last':last_price[worst_index], 'err':hedging_error[worst_index].reshape((-1, ))})
 pd.DataFrame({'last':last_price[worst_index], 'err':hedging_error[worst_index].reshape((-1, ))})
 
@@ -22,7 +24,7 @@ plt.plot(worst_ts)
 plt.show(block=False)
 
 # うまくいった時系列
-best_index = np.argsort(hedging_error, axis=0)[-10:].reshape(-1, )
+best_index = np.argsort(hedging_error, axis=0)[-n_pickup:].reshape(-1, )
 pd.DataFrame({'last':last_price[best_index], 'err':hedging_error[best_index].reshape((-1, ))})
 pd.DataFrame({'last':last_price[best_index], 'err':hedging_error[best_index].reshape((-1, ))})
 
@@ -33,10 +35,26 @@ plt.figure()
 plt.plot(best_ts)
 plt.show(block=False)
 
+# 正確な時系列
+precise_index = np.argsort(np.abs(hedging_error), axis=0)[:n_pickup].reshape(-1, )
+pd.DataFrame({'last':last_price[precise_index], 'err':hedging_error[precise_index].reshape((-1, ))})
+pd.DataFrame({'last':last_price[precise_index], 'err':hedging_error[precise_index].reshape((-1, ))})
+
+input_array = np.stack(input_list)
+precise_ts = input_array[:, precise_index]
+
 plt.figure()
-plt.scatter(input_list[-1], hedged_payoff)
-plt.grid()
+plt.plot(precise_ts)
 plt.show(block=False)
 
-option_price = model_option_price.predict(input_list)
-print(option_price[0][0])
+
+# ヘッジ誤差が大きい時系列とヘッジ誤差が小さい時系列とでは傾向が異なる
+col_alpha = 0.1
+plt.figure()
+plt.plot(worst_ts, color=(1, 0, 0, col_alpha))
+plt.plot(best_ts, color=(0, 1, 0, col_alpha))
+plt.plot(precise_ts, color=(0, 0, 1, col_alpha))
+plt.plot(worst_ts.mean(axis=1), color=(1, 0, 0, 1))
+plt.plot(best_ts.mean(axis=1), color=(0, 1, 0, 1))
+plt.plot(precise_ts.mean(axis=1), color=(0, 0, 1, 1))
+plt.show(block=False)
